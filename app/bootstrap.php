ed9f3fddef29a394ad81880b835ccc1f07a457c6
VladaHejda <hejdav@centrum.cz>
2015-03-31 23:41:56 +0200

    home page

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,30 @@
 <?php
+
+use Nette\Application\Routers\Route;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+$configurator->enableDebugger(__DIR__ . '/../log');
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->addConfig(__DIR__ . '/config/secure.neon', $configurator::AUTO);
+$configurator->addConfig(__DIR__ . '/config/services.neon');
+$container = $configurator->createContainer();
+
+$router = $container->getService('router');
+
+$router[] = new Route('', function($presenter) use ($container) {
+
+	$template = $presenter->createTemplate()->setFile(__DIR__ . '/templates/home.latte');
+	$template->setParameters(array(
+		'countdown' => (object) array(
+			'days' => 0,
+			'hours' => 0,
+			'minutes' => 0,
+			'seconds' => 13,
+		),
+	));
+	return $template;
+});
+
+$container->getService('application')->run();
